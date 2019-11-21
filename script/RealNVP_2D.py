3460110414fea21f57a7a42c2572a75128cceefb
Xinqiang Ding <xqding@umich.edu>
2019-11-20 22:45:54 -0500

    add plots for moon dataset

--- script/RealNVP_2D.py
+++ script/RealNVP_2D.py
@@ -29,19 +29,22 @@ class Affine_Coupling(nn.Module):
         self.translation_fc2 = nn.Linear(self.hidden_dim, self.hidden_dim)
         self.translation_fc3 = nn.Linear(self.hidden_dim, self.input_dim)
 
-    def _compute_scale(self, x):        
+    def _compute_scale(self, x):
+        ## compute scaling factor using unchanged part of x with a neural network
         s = torch.relu(self.scale_fc1(x*self.mask))
         s = torch.relu(self.scale_fc2(s))
         s = torch.relu(self.scale_fc3(s)) * self.scale        
         return s
 
     def _compute_translation(self, x):
+        ## compute translation using unchanged part of x with a neural network        
         t = torch.relu(self.translation_fc1(x*self.mask))
         t = torch.relu(self.translation_fc2(t))
         t = self.translation_fc3(t)        
         return t
     
     def forward(self, x):
+        ## convert latent space variable to observed variable
         s = self._compute_scale(x)
         t = self._compute_translation(x)
         
@@ -51,6 +54,7 @@ class Affine_Coupling(nn.Module):
         return y, logdet
 
     def inverse(self, y):
+        ## convert observed varible to latent space variable
         s = self._compute_scale(y)
         t = self._compute_translation(y)
                 
@@ -60,9 +64,16 @@ class Affine_Coupling(nn.Module):
         return x, logdet
 
     
-class RealNVP_2D(nn.Module):    
+class RealNVP_2D(nn.Module):
+    '''
+    A vanilla RealNVP class for modeling 2 dimensional distributions
+    '''
+    
     def __init__(self, masks, hidden_dim):
-        super(RealNVP_2D, self).__init__()
+        '''
+        initialized with a list of masks. each mask define an affine coupling layer
+        '''
+        super(RealNVP_2D, self).__init__()        
         self.hidden_dim = hidden_dim        
         self.masks = nn.ParameterList(
             [nn.Parameter(torch.Tensor(m),requires_grad = False)
@@ -73,12 +84,15 @@ class RealNVP_2D(nn.Module):
              for i in range(len(self.masks))])
         
     def forward(self, x):
+        ## convert latent space variables into observed variables
         y = x
         logdet_tot = 0
         for i in range(len(self.affine_couplings)):
             y, logdet = self.affine_couplings[i](y)
             logdet_tot = logdet_tot + logdet
 
+        ## a normalization layer is added such that the observed variables is within
+        ## the range of [-4, 4].
         logdet = torch.sum(torch.log(torch.abs(4*(1-(torch.tanh(y))**2))), -1)        
         y = 4*torch.tanh(y)
         logdet_tot = logdet_tot + logdet
@@ -86,13 +100,16 @@ class RealNVP_2D(nn.Module):
         return y, logdet_tot
 
     def inverse(self, y):
+        ## convert observed variables into latent space variables        
         x = y        
         logdet_tot = 0
 
+        # inverse the normalization layer
         logdet = torch.sum(torch.log(torch.abs(1.0/4.0* 1/(1-(x/4)**2))), -1)
         x  = 0.5*torch.log((1+x/4)/(1-x/4))
         logdet_tot = logdet_tot + logdet
-        
+
+        ## inverse affine coupling layers
         for i in range(len(self.affine_couplings)-1, -1, -1):
             x, logdet = self.affine_couplings[i].inverse(x)
             logdet_tot = logdet_tot + logdet
--- script/learn_by_data.py
+++ script/learn_by_data.py
@@ -56,7 +56,7 @@ for idx_step in range(num_steps):
 
     if (idx_step + 1) % 100 == 0:
         print(f"idx_steps: {idx_step:}, loss: {loss.item():.5f}")
-
+        
 ## after learning, we can test if the model can transform
 ## the moon data distribution into the normal distribution
 X, label = datasets.make_moons(n_samples = 1000, noise = 0.05)
@@ -64,20 +64,43 @@ X = torch.Tensor(X).to(device = device)
 z, logdet_jacobian = realNVP.inverse(X)
 z = z.cpu().detach().numpy()
 
-fig = plt.figure(0)
+X = X.cpu().detach().numpy()
+fig = plt.figure(2, figsize = (12.8, 4.8))
 fig.clf()
+plt.subplot(1,2,1)
+plt.plot(X[label==0,0], X[label==0,1], ".")
+plt.plot(X[label==1,0], X[label==1,1], ".")
+plt.title("X sampled from Moon dataset")
+plt.xlabel(r"$x_1$")
+plt.ylabel(r"$x_2$")
+
+plt.subplot(1,2,2)
 plt.plot(z[label==0,0], z[label==0,1], ".")
 plt.plot(z[label==1,0], z[label==1,1], ".")
+plt.title("Z transformed from X")
+plt.xlabel(r"$z_1$")
+plt.ylabel(r"$z_2$")
 plt.savefig("./output/moon_z_transformed_from_x.pdf")
 
 ## after learning, we can also test if the model can transform
 ## the normal distribution into the moon data distribution 
 z = torch.normal(0, 1, size = (1000, 2)).to(device = device)
-x, _ = realNVP(z)
-x = x.cpu().detach().numpy()
+X, _ = realNVP(z)
+X = X.cpu().detach().numpy()
+z = z.cpu().detach().numpy()
 
-fig = plt.figure(0)
+fig = plt.figure(2, figsize = (12.8, 4.8))
 fig.clf()
-plt.plot(x[:,0], x[:,1], ".")
+plt.subplot(1,2,1)
+plt.plot(z[:,0], z[:,1], ".")
+plt.title("Z sampled from normal distribution")
+plt.xlabel(r"$z_1$")
+plt.ylabel(r"$z_2$")
+
+plt.subplot(1,2,2)
+plt.plot(X[:,0], X[:,1], ".")
+plt.title("X transformed from Z")
+plt.xlabel(r"$x_1$")
+plt.ylabel(r"$x_2$")
 plt.savefig("./output/moon_x_transformed_from_z.pdf")
 
